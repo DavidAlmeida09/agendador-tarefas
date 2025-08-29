4042c9fb5a1176373545453d3e6031ae0f967eba
David Almeida <davidalmeida099@gmail.com>
2025-08-29 16:00:05 -0300

    adcionados métodos get por email e por periodo

--- src/main/java/com/agendador/agendadorTarefas/business/mapper/TarefasConverter.java
+++ src/main/java/com/agendador/agendadorTarefas/business/mapper/TarefasConverter.java
@@ -4,9 +4,14 @@ import com.agendador.agendadorTarefas.business.dto.TarefasDTO;
 import com.agendador.agendadorTarefas.infrastructure.entity.TarefasEntity;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface TarefasConverter {
 
     TarefasEntity paraTarefaEntity(TarefasDTO dto);
     TarefasDTO paraTarefaDTO(TarefasEntity entity);
+
+    List<TarefasEntity> paraListaTarefasEntity(List<TarefasDTO> dtos);
+    List<TarefasDTO> paraListaTarefasDTO(List<TarefasEntity> entities);
 }
